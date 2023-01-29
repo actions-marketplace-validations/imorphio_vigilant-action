6ff061af2119bb21db1c180a93e88f791931a927
anuragteapot <anuragvns1999@gmail.com>
2023-01-29 19:06:22 +0530

    feat: auto publish check

--- src/action.ts
+++ src/action.ts
@@ -5,32 +5,60 @@ import { Archiver } from "./archiver";
 import { ImorphClient } from "./axios";
 
 export async function pre(options: GitHubOptions): Promise<void> {
-  core.info("Imorph pre build setup...");
-  const imorphClient = new ImorphClient(options);
-  await imorphClient.create();
+  try {
+    core.info("Imorph pre build setup...");
+    const imorphClient = new ImorphClient(options);
+    const data = await imorphClient.create();
+    core.saveState("APP_BUILD_ID", data.id);
+  } catch (err) {
+    if (err instanceof Error) {
+      core.setFailed(err);
+    } else {
+      core.setFailed("Something went wrong");
+    }
+  }
 }
 
 export async function main(options: GitHubOptions): Promise<void> {
-  core.info("Imorph main build setup...");
+  try {
+    core.info("Imorph main build setup...");
+  } catch (err) {
+    if (err instanceof Error) {
+      core.setFailed(err);
+    } else {
+      core.setFailed("Something went wrong");
+    }
+  }
 }
 
 export async function post(options: GitHubOptions): Promise<void> {
-  core.info("Imorph post build setup...");
-  const buildDir = options.buildDir;
+  try {
+    core.info("Imorph post build setup...");
+    const buildDir = options.buildDir;
 
-  if (!existsSync(buildDir)) {
-    core.setFailed(`${buildDir} not found`);
-    return;
+    if (!existsSync(buildDir)) {
+      core.setFailed(`${buildDir} not found`);
+      return;
+    }
+
+    if (!existsSync(`${buildDir}/index.html`)) {
+      core.setFailed(
+        `${buildDir} your build directory should contain index.html in root`
+      );
+      return;
+    }
+
+    const archiver = new Archiver(buildDir);
+    const output = await archiver.start();
+    const imorphClient = new ImorphClient(options);
+    const id = core.getState("APP_BUILD_ID");
+    const data = await imorphClient.status(id);
+    console.log(statSync(output), data);
+  } catch (err) {
+    if (err instanceof Error) {
+      core.setFailed(err);
+    } else {
+      core.setFailed("Something went wrong");
+    }
   }
-
-  if (!existsSync(`${buildDir}/index.html`)) {
-    core.setFailed(
-      `${buildDir} your build directory should contain index.html in root`
-    );
-    return;
-  }
-
-  const archiver = new Archiver(buildDir);
-  const output = await archiver.start();
-  console.log(statSync(output));
 }
--- src/axios.ts
+++ src/axios.ts
@@ -4,26 +4,34 @@ import { GitHubOptions } from "./options";
 export class ImorphClient {
   private axios: AxiosInstance;
   private options: GitHubOptions;
+
   constructor(options: GitHubOptions) {
-    this.axios = axios.create({ headers: { "im-vigilant-action": true } });
+    const { imorphDomain, imorphSecret } = options;
+    const baseURL = `https://${imorphDomain}/api/vigilantaction/${imorphSecret}/${imorphDomain}`;
+    this.axios = axios.create({
+      baseURL,
+      headers: { "im-vigilant-action": true },
+    });
     this.options = options;
   }
 
   async create() {
-    const {
-      imorphDomain,
-      imorphSecret,
-      githubRepo,
-      githubRunId,
-      githubRef,
-      githubEvent,
-    } = this.options;
-
-    const url = `https://${imorphDomain}/api/vigilantaction/${imorphSecret}/${imorphDomain}/create`;
+    const { githubRepo, githubRunId, githubRef, githubEvent } = this.options;
+    const url = `/create`;
     const title = `${githubRepo} : ${githubRef} : ${githubEvent}`;
     const jobHostname = githubRepo;
     const jobId = githubRunId;
     const { data } = await this.axios.post(url, { title, jobHostname, jobId });
+    return data;
+  }
+
+  async status(id: string) {
+    const { imorphDomain } = this.options;
+    const url = `/status`;
+    const buildId = `${imorphDomain}/${id}`;
+    const action = "AUTO_PUBLISH";
+    const status = "SUCCESS";
+    const { data } = await this.axios.post(url, { buildId, action, status });
     return data;
   }
 }
